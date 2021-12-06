2c0a928340aeab45f5adcee2550683ec47e5b579
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-12-06 14:37:08 -0600

    ...

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 console.log("Hello World!");   
                                                                                      
+document.querySelector("#menu").addEventListener('click' , () => {
+    document.querySelector('nav ul').classList.toggle('showMenu')
+})
